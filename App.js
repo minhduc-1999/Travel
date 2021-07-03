8c28c7b6187d3eaaa692addba2658047685b4a4d
MinhDuc <minhduc9111999@gmail.com>
2021-07-03 21:25:03 +0700

    bootsplash

--- App.js
+++ App.js
@@ -6,14 +6,19 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import Providers from './src/navigation/index';
 import Toast from 'react-native-toast-message';
 import toastConfig from './src/components/CustomToast';
 import 'react-native-gesture-handler';
 import RemotePushController from './src/Services/RemotePushController';
+import RNBootSplash from 'react-native-bootsplash';
+
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true, duration: 250});
+  }, []);
   return (
     <>
       <StatusBar barStyle="light-content" />
--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,18 +1,72 @@
-import React, {useState} from 'react';
-import {ActivityIndicator, SafeAreaView, StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StatusBar, View, Image} from 'react-native';
+import {windowWidth} from '../../Utils/Dimention';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+  withRepeat,
+  Easing,
+} from 'react-native-reanimated';
 
 const Splash = props => {
-  console.log('Splash screen render');
+  const rotation = useSharedValue(0);
+  const zoom = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      // transform: [{rotateZ: `${rotation.value}deg`}],
+      transform: [{scale: zoom.value}],
+    };
+  });
+  useEffect(() => {
+    // rotation.value = withSequence(
+    //   withTiming(-5, {duration: 1000}),
+    //   withRepeat(withTiming(5, {duration: 1000}), -1, true),
+    //   withTiming(0, {duration: 1000}),
+    // );
+    // zoom.value = withTiming(
+    //   20,
+    //   {duration: 1500, easing: Easing.bezier(0.32, 0, 0.67, 0)},
+    //   isFinish => {
+    //     console.log(isFinish);
+    //   },
+    // );
+  }, []);
   return (
     <SafeAreaView
       style={{
+        backgroundColor: '#fff',
         width: '100%',
         height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <StatusBar hidden />
-      <ActivityIndicator color="#f15454" size={50} />
+      <StatusBar
+        translucent
+        barStyle="dark-content"
+        backgroundColor="transparent"
+      />
+      <Animated.View
+        style={[
+          {
+            backgroundColor: '#fff',
+            height: windowWidth * 0.35,
+            width: windowWidth * 0.35,
+            borderRadius: (windowWidth / 2) * 0.35,
+            justifyContent: 'center',
+            alignItems: 'center',
+            overflow: 'hidden',
+          },
+          animatedStyle,
+        ]}>
+        <Animated.Image
+          width={windowWidth * 0.18}
+          height={windowWidth * 0.18}
+          source={require('../../../assets/images/Travelad.png')}
+        />
+      </Animated.View>
     </SafeAreaView>
   );
 };
